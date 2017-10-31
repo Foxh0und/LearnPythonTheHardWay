dcb8dfbef98aadf7f1d5386813da79334dc60146
Foxh0und <amiritis@wolfmail.co>
2017-10-31 12:06:43 +1100

    Create ex16.py

---
+++ Exercises/ex16.py
@@ -0,0 +1,40 @@
+from sys import arv
+
+script, filename = argv
+
+print( f"We're going to errase {filename} (if it exists)." )
+print( "If you don't wish to do that, hit CTRL-C" )
+print( "If you're happy to do that, press return" )
+
+input( "?" )
+
+print( "Opening the file..." )
+
+target = open( filename, 'w' )
+
+print( "Truncating the file. Goodbye!" )
+target.truncate()
+
+print( "Now I'm going to ask you for three lines." )
+
+line1 = input( "line 1: " )
+line2 = input( "line 2: " )
+line3 = input( "line 3: " )
+
+print( "I'm now going to write these to the file!" )
+
+target.write( line1 )
+target.write( "\n" )
+
+target.write( line2 )
+target.write( "\n" )
+
+target.write( line3 )
+target.write( "\n" )
+
+print( "And finally we close the file. Let's take a look and see what's inside!" )
+
+file = open( filename )
+print( file.read() )
+
+print( "See, it worked really well!" )
