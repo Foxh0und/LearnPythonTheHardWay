5f0d432b0fd90cd6f6d2ff9c130c7ef3cd549402
Foxh0und <amiritis@wolfmail.co>
2017-12-18 10:57:39 +1100

    Update ex47_tests.py

--- Exercises/47/tests/ex47_tests.py
+++ Exercises/47/tests/ex47_tests.py
@@ -7,3 +7,16 @@ def test_room():
                       There's a door to the north.""")
     assert_equal( lTestRoom.fName, "GoldRoom" )
     assert_equal( lTestRoom.fPaths, {} )
+
+def test_room_paths():
+    lCenter = Room( "Center", "Test room in the center." )
+    lNorth = Room( "North", "Test room in the north." )
+    lSouth = Room( "South", "Test room in the south." )
+
+    lCenter.addPaths( {'north': lNorth, 'south': lSouth} )
+    assert_equal( lCenter.go( 'north' ), lNorth )
+    assert_equal( lCenter.go( 'south' ), lSouth )
+
+  
+
+        
